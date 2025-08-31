5e340164f4eaa218eca93699c7ff80ca51b7a244
Alex Bertotto <ajbertotto@gmail.com>
2025-08-31 11:11:32 -0700

    use a for loop and and conditional to check the condition presented by the finger exercise

--- lec12.py
+++ lec12.py
@@ -6,6 +6,12 @@ def count_sqrts(nums_list):
     Returns how many elements in nums_list are exact squares of elements in the same list, including itself.
     """
     # Your code here
+    count = 0
+    for num in nums_list:
+        if num*num in nums_list:
+            count += 1
+            
+    return count
 
 # Examples:    
 print(count_sqrts([3,4,2,1,9,25])) # prints 3
